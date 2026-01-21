c2b7244d4baf1e55aa95ede86d4ca9e39e92505f
ko gyeong tae <rudex1598@gmail.com>
2026-01-21 16:33:08 +0900

    [Add] Init, 기본 베이스 3인칭 탬플릿 추가

---
+++ necromancer/Source/necromancer/necromancer.Build.cs
@@ -0,0 +1,51 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class necromancer : ModuleRules
+{
+	public necromancer(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"necromancer",
+			"necromancer/Variant_Platforming",
+			"necromancer/Variant_Platforming/Animation",
+			"necromancer/Variant_Combat",
+			"necromancer/Variant_Combat/AI",
+			"necromancer/Variant_Combat/Animation",
+			"necromancer/Variant_Combat/Gameplay",
+			"necromancer/Variant_Combat/Interfaces",
+			"necromancer/Variant_Combat/UI",
+			"necromancer/Variant_SideScrolling",
+			"necromancer/Variant_SideScrolling/AI",
+			"necromancer/Variant_SideScrolling/Gameplay",
+			"necromancer/Variant_SideScrolling/Interfaces",
+			"necromancer/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ necromancer/Source/necromancer/necromancerGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "necromancerGameMode.h"
+
+AnecromancerGameMode::AnecromancerGameMode()
+{
+	// stub
+}
