185569e4389566e9b79631ded8a8e2e5f98bd4a0
pyosounds <156515622+pyosounds@users.noreply.github.com>
2026-01-23 01:33:15 +0900

    [Add] PlayerState 기초 작업

--- Source/necromancer/Game/NecPlayerState.cpp
+++ Source/necromancer/Game/NecPlayerState.cpp
@@ -1,2 +1,17 @@
 #include "Game/NecPlayerState.h"
+#include "GAS/Base/BaseAttributeSet.h"
+#include "AbilitySystemComponent.h"
 
+ANecPlayerState::ANecPlayerState()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed); // Temp
+
+	AttributeSet = CreateDefaultSubobject<UBaseAttributeSet>(TEXT("AttributeSet"));
+}
+
+UAbilitySystemComponent* ANecPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
--- Source/necromancer/Game/NecPlayerState.h
+++ Source/necromancer/Game/NecPlayerState.h
@@ -2,11 +2,27 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
 #include "NecPlayerState.generated.h"
 
+class UBaseAttributeSet;
+
 UCLASS()
-class NECROMANCER_API ANecPlayerState : public APlayerState
+class NECROMANCER_API ANecPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 	
+public:
+	ANecPlayerState();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	UBaseAttributeSet* GetAttributeSet() const { return AttributeSet; }
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UBaseAttributeSet> AttributeSet;
 };
