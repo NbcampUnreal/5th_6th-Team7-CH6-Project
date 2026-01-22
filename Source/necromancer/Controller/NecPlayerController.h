bd132bba321af5095120b96eb4df40fea58845ae
pyosounds <156515622+pyosounds@users.noreply.github.com>
2026-01-22 17:57:55 +0900

    [Add] NecPlayerController 추가

---
+++ Source/necromancer/Controller/NecPlayerController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "NecPlayerController.generated.h"
+
+UCLASS()
+class NECROMANCER_API ANecPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
