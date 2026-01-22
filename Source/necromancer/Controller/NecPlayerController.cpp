1ce6bb59ea75fa309b2826f710d88118be72e4c6
pyosounds <156515622+pyosounds@users.noreply.github.com>
2026-01-22 19:30:28 +0900

    [Add] 플레이어 캐릭터 기초 이동 작업

--- Source/necromancer/Controller/NecPlayerController.cpp
+++ Source/necromancer/Controller/NecPlayerController.cpp
@@ -1,2 +1,27 @@
 #include "Controller/NecPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+#include "InputMappingContext.h"
+#include "Blueprint/UserWidget.h"
+#include "necromancer.h"
+#include "Widgets/Input/SVirtualJoystick.h"
 
+void ANecPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+		
+	
+}
+
+void ANecPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (IsLocalPlayerController())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContexts, 0);
+		}
+	}
+}
--- Source/necromancer/Controller/NecPlayerController.h
+++ Source/necromancer/Controller/NecPlayerController.h
@@ -4,9 +4,21 @@
 #include "GameFramework/PlayerController.h"
 #include "NecPlayerController.generated.h"
 
+class UInputMappingContext;
+class UUserWidget;
+
 UCLASS()
 class NECROMANCER_API ANecPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+protected:	
+
+	virtual void BeginPlay() override;
+
+	virtual void SetupInputComponent() override;	
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Input|Input Mappings")
+	UInputMappingContext* DefaultMappingContexts;
 };
