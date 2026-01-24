332c9e7e54abc59b35cd14e6a74d6fb2b44befdf
pyosounds <156515622+pyosounds@users.noreply.github.com>
2026-01-24 15:19:09 +0900

    [Mod] Build.cs 수정

--- Source/Necromancer/Necromancer.Build.cs
+++ Source/Necromancer/Necromancer.Build.cs
@@ -8,6 +8,22 @@ public class Necromancer : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput",
+            "AIModule",
+            "NavigationSystem",
+            "GameplayTasks",
+            "UMG",
+            "Niagara"
+        });
+
+        PublicIncludePaths.AddRange(new string[]
+        {
+            "Necromancer",
+        });
+    }
 }
