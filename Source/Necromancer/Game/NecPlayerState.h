98abea1a19a798293ec05cbb0445cbeb05e6538a
pyosounds <156515622+pyosounds@users.noreply.github.com>
2026-01-26 16:46:49 +0900

    [Add] NecPlayerState 클래스 추가

---
+++ Source/Necromancer/Game/NecPlayerState.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "NecPlayerState.generated.h"
+
+UCLASS()
+class NECROMANCER_API ANecPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
