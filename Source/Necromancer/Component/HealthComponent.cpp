fae6f3386e4d337cc334a7bf66639fe36e110eef
pyosounds <156515622+pyosounds@users.noreply.github.com>
2026-01-27 16:16:14 +0900

    [Add] HealthComponent 기초 로직 추가

--- Source/Necromancer/Component/HealthComponent.cpp
+++ Source/Necromancer/Component/HealthComponent.cpp
@@ -1,22 +1,104 @@
 #include "Component/HealthComponent.h"
+#include "GameFramework/Actor.h"
+#include "Net/UnrealNetwork.h"
+#include "Kismet/GameplayStatics.h"
 
 UHealthComponent::UHealthComponent()
+	: CurrentHealth(0.0f)
 {
-	PrimaryComponentTick.bCanEverTick = true;
-
-	
+	SetIsReplicatedByDefault(true);
 }
 
 void UHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
+    if (GetOwner()->HasAuthority())
+    {
+        GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::HandleTakeDamage);
+
+        CurrentHealth = MaxHealth;
+
+        OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
+    }
 }
 
-void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UHealthComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
-	
+    DOREPLIFETIME(UHealthComponent, CurrentHealth);
+    DOREPLIFETIME(UHealthComponent, MaxHealth);
+    DOREPLIFETIME(UHealthComponent, Armor);
+}
+
+void UHealthComponent::OnRep_Health()
+{
+    OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
+}
+
+void UHealthComponent::HandleTakeDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
+{
+    if (Damage <= 0.0f || CurrentHealth <= 0.0f)
+    {
+        return;
+    }
+
+    float ActualDamage = FMath::Max(Damage - Armor, 0.0f);
+    float NewHealth = FMath::Clamp(CurrentHealth - ActualDamage, 0.0f, MaxHealth);
+
+    SetCurrentHealth(NewHealth);
+    OnDamageReceived.Broadcast(ActualDamage, GetOwner()->GetActorLocation());
+
+    if (NewHealth <= 0.0f)
+    {
+        OnDeath.Broadcast();
+    }
+}
+
+void UHealthComponent::SetCurrentHealth(float NewHealth)
+{
+    if (!GetOwner()->HasAuthority())
+    {
+        return;
+    }
+
+    CurrentHealth = NewHealth;
+
+    OnRep_Health();
+}
+
+void UHealthComponent::AddMaxHealth(float Amount)
+{
+    if (!GetOwner()->HasAuthority() || Amount == 0.0f)
+    {
+        return;
+    }
+
+    MaxHealth += Amount;
+    CurrentHealth += Amount;
+
+    OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
+}
+
+void UHealthComponent::AddArmor(float Amount)
+{
+    if (!GetOwner()->HasAuthority() || Amount == 0.0f)
+    {
+        return;
+    }
+
+    Armor += Amount;
+}
+
+void UHealthComponent::Heal(float HealAmount)
+{
+    if (!GetOwner()->HasAuthority() || HealAmount <= 0.0f || CurrentHealth <= 0.0f)
+    {
+        return;
+    }
+
+    float NewHealth = FMath::Clamp(CurrentHealth + HealAmount, 0.0f, MaxHealth);
+
+    SetCurrentHealth(NewHealth);
 }
--- Source/Necromancer/Component/HealthComponent.h
+++ Source/Necromancer/Component/HealthComponent.h
@@ -4,19 +4,65 @@
 #include "Components/ActorComponent.h"
 #include "HealthComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHealthChangedSignature, float, CurrentHealth, float, MaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnDamageReceivedSignature, float, DamageAmount, FVector, HitLocation);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class NECROMANCER_API UHealthComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:		
+public:
 	UHealthComponent();
 
-protected:	
+protected:
 	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;        
 
-public:		
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+    UFUNCTION()
+    void OnRep_Health();
 
+    UFUNCTION()
+    void HandleTakeDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);        
+
+    void SetCurrentHealth(float NewHealth);
+
+public:
+    UFUNCTION(BlueprintPure)
+    float GetCurrentHealth() const { return CurrentHealth; }
+
+    UFUNCTION(BlueprintPure)
+    float GetMaxHealth() const { return MaxHealth; }
+
+    UFUNCTION(BlueprintPure)
+    float GetArmor() const { return Armor; }
+
+    UFUNCTION(BlueprintCallable)
+    void AddMaxHealth(float Amount);
+
+    UFUNCTION(BlueprintCallable)
+    void AddArmor(float Amount);
+
+    UFUNCTION(BlueprintCallable)
+    void Heal(float HealAmount);
+
+    UPROPERTY(BlueprintAssignable)
+    FOnHealthChangedSignature OnHealthChanged;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnDamageReceivedSignature OnDamageReceived;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnDeathSignature OnDeath;
+
+protected:
+    UPROPERTY(ReplicatedUsing = OnRep_Health, VisibleAnywhere, Category = "Health")
+    float CurrentHealth;
+
+    UPROPERTY(ReplicatedUsing = OnRep_Health, EditAnywhere, Category = "Health", meta = (ClampMin = "1.0"))
+    float MaxHealth = 100.0f;
+
+    UPROPERTY(Replicated, EditAnywhere, Category = "Armor", meta = (ClampMin = "0.0"))
+    float Armor = 0.0f;
 };
