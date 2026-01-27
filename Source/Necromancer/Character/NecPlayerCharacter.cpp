809b2c3455fbdc2c6743bb7a2d63d9b13d6ffd2c
pyosounds <156515622+pyosounds@users.noreply.github.com>
2026-01-27 19:42:45 +0900

    [Add] PlayerCharacter 기초 로직 추가

--- Source/Necromancer/Character/NecPlayerCharacter.cpp
+++ Source/Necromancer/Character/NecPlayerCharacter.cpp
@@ -1,9 +1,30 @@
 #include "Character/NecPlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Controller/NecPlayerController.h"
+#include "EnhancedInputComponent.h"
 
 ANecPlayerCharacter::ANecPlayerCharacter()
 {
- 	PrimaryActorTick.bCanEverTick = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
 
+	SprintSpeed = NormalSpeed * SprintSpeedMultiplier;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+	GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->TargetArmLength = 300.0f;
+	SpringArmComp->bUsePawnControlRotation = true;
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
+	CameraComp->bUsePawnControlRotation = false;	
 }
 
 void ANecPlayerCharacter::BeginPlay()
@@ -12,14 +33,146 @@ void ANecPlayerCharacter::BeginPlay()
 	
 }
 
-void ANecPlayerCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
 void ANecPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (ANecPlayerController* PlayerController = Cast<ANecPlayerController>(GetController()))
+		{
+			if (PlayerController->MoveAction)
+			{
+				EnhancedInputComp->BindAction(
+					PlayerController->MoveAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ANecPlayerCharacter::Move
+				);
+			}
+
+			if (PlayerController->LookAction)
+			{
+				EnhancedInputComp->BindAction(
+					PlayerController->LookAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ANecPlayerCharacter::Look
+				);
+			}
+
+			if (PlayerController->SprintAction)
+			{
+				EnhancedInputComp->BindAction(
+					PlayerController->SprintAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ANecPlayerCharacter::StartSprint
+				);
+
+				EnhancedInputComp->BindAction(
+					PlayerController->SprintAction,
+					ETriggerEvent::Completed,
+					this,
+					&ANecPlayerCharacter::StopSprint
+				);
+			}
+
+			if (PlayerController->AttackAction)
+			{
+				EnhancedInputComp->BindAction(
+					PlayerController->AttackAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ANecPlayerCharacter::Attack
+				);
+			}
+
+			if (PlayerController->GuardAction)
+			{
+				EnhancedInputComp->BindAction(
+					PlayerController->GuardAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ANecPlayerCharacter::Guard
+				);
+			}
+
+			if (PlayerController->LockonAction)
+			{
+				EnhancedInputComp->BindAction(
+					PlayerController->LockonAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ANecPlayerCharacter::Lockon
+				);
+			}
+		}
+	}	
+}
+
+void ANecPlayerCharacter::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{		
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+				
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);		
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+				
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void ANecPlayerCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{		
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void ANecPlayerCharacter::StartSprint(const FInputActionValue& Value)
+{
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+		Server_SetMaxWalkSpeed(SprintSpeed);
+	}	
+}
+
+void ANecPlayerCharacter::StopSprint(const FInputActionValue& Value)
+{
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
+		Server_SetMaxWalkSpeed(NormalSpeed);
+	}
+}
+
+void ANecPlayerCharacter::Attack(const FInputActionValue& Value)
+{
+
+}
+
+void ANecPlayerCharacter::Guard(const FInputActionValue& Value)
+{
+
+}
+
+void ANecPlayerCharacter::Lockon(const FInputActionValue& Value)
+{
+
+}
+
+void ANecPlayerCharacter::Server_SetMaxWalkSpeed_Implementation(float NewSpeed)
+{
+	GetCharacterMovement()->MaxWalkSpeed = NewSpeed;
 }
--- Source/Necromancer/Character/NecPlayerCharacter.h
+++ Source/Necromancer/Character/NecPlayerCharacter.h
@@ -4,6 +4,10 @@
 #include "GameFramework/Character.h"
 #include "NecPlayerCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+struct FInputActionValue;
+
 UCLASS()
 class NECROMANCER_API ANecPlayerCharacter : public ACharacter
 {
@@ -13,9 +17,46 @@ public:
 	ANecPlayerCharacter();
 
 protected:	
-	virtual void BeginPlay() override;	
-	virtual void Tick(float DeltaTime) override;		
+	virtual void BeginPlay() override;		
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
 
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void StartSprint(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void StopSprint(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void Attack(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void Guard(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void Lockon(const FInputActionValue& Value);
+
+	UFUNCTION(Server, Reliable)
+	void Server_SetMaxWalkSpeed(float NewSpeed);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float NormalSpeed = 400.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float SprintSpeedMultiplier = 1.7f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	float SprintSpeed;
 };
