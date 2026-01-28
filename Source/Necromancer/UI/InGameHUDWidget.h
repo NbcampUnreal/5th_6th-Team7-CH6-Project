8949504889f9b7f26d3951850d6c68a132f232b9
pyosounds <156515622+pyosounds@users.noreply.github.com>
2026-01-28 11:00:46 +0900

    [Add] InGameHUDWidget 클래스 추가/폴더 구조 변경

---
+++ Source/Necromancer/UI/InGameHUDWidget.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InGameHUDWidget.generated.h"
+
+UCLASS()
+class NECROMANCER_API UInGameHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
