40d244d86a3cde59ad72d418153dcdb9d819f08f
zerg8474 <zerg8474@gmail.com>
2026-02-04 16:08:14 +0900

    1

--- Source/Necromancer/Item/ItemBass.cpp
+++ Source/Necromancer/Item/ItemBass.cpp
@@ -1,14 +1,50 @@
-#include "ItemBass.h"
-#include "GridInventory/ItemInstance/ItemInstanceComponent.h"
+// ItemBass.cpp
+
+#include "Item/ItemBass.h"
+#include "Components/SceneComponent.h"
+#include "Engine/World.h"
 
 AItemBass::AItemBass()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	USceneComponent* SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	SetRootComponent(SceneComp);
+
 	ItemInstanceComponent = CreateDefaultSubobject<UItemInstanceComponent>(TEXT("ItemInstanceComponent"));
+}
+
+bool AItemBass::AddCount(int32 Amount)
+{
+	if (Amount <= 0)
+	{
+		return false;
+	}
+
+	ItemData.Count += Amount;
+	return true;
+}
+
+bool AItemBass::RemoveCount(int32 Amount)
+{
+	if (Amount <= 0 || ItemData.Count <= 0)
+	{
+		return false;
+	}
+
+	ItemData.Count -= Amount;
+
+	if (ItemData.Count < 0)
+	{
+		ItemData.Count = 0;
+	}
+
+	return true;
 }
 
 void AItemBass::OnDropped(const FVector& WorldLocation)
 {
 	SetActorLocation(WorldLocation);
+
+	UE_LOG(LogTemp, Log, TEXT("Item %s dropped at location: %s"), *ItemData.ItemID.ToString(), *WorldLocation.ToString());
 }
