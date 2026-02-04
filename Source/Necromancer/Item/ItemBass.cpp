4e5067848aeb3bd4a91e326254745e58c8a05fc5
zerg8474 <zerg8474@gmail.com>
2026-02-04 15:55:55 +0900

    임시

--- Source/Necromancer/Item/ItemBass.cpp
+++ Source/Necromancer/Item/ItemBass.cpp
@@ -1,52 +1,14 @@
-//ItemBass.cpp
-
 #include "ItemBass.h"
-#include "Net/UnrealNetwork.h"
-#include "GridInventory/ItemInstance/ItemInstance.h"
-#include "GridInventory/ItemData/ItemData.h"
+#include "GridInventory/ItemInstance/ItemInstanceComponent.h"
 
 AItemBass::AItemBass()
 {
-	bReplicates = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	ItemInstanceComponent = CreateDefaultSubobject<UItemInstanceComponent>(TEXT("ItemInstanceComponent"));
 }
 
-void AItemBass::CopyFromItemInstance(UItemInstance* ItemInstance)
+void AItemBass::OnDropped(const FVector& WorldLocation)
 {
-	if (!ItemInstance)
-	{
-		return;
-	}
-
-	InstanceID = ItemInstance->InstanceID;
-	ItemID = ItemInstance->ItemID;
-	CurrentDurability = ItemInstance->CurrentDurability;
-	bRotated = ItemInstance->bRotated;
-	OwnerItemGuid = ItemInstance->OwnerItemGuid;
-	SectionIndex = ItemInstance->SectionIndex;
-	PosX = ItemInstance->PosX;
-	PosY = ItemInstance->PosY;
-}
-
-const FItemData* AItemBass::GetItemData() const
-{
-	if (!ItemDataTable || ItemID.IsNone())
-	{
-		return nullptr;
-	}
-
-	return ItemDataTable->FindRow<FItemData>(ItemID,TEXT("ItemBass::GetItemData"));
-}
-
-void AItemBass::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
-{
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-
-	DOREPLIFETIME(AItemBass, InstanceID);
-	DOREPLIFETIME(AItemBass, ItemID);
-	DOREPLIFETIME(AItemBass, CurrentDurability);
-	DOREPLIFETIME(AItemBass, bRotated);
-	DOREPLIFETIME(AItemBass, OwnerItemGuid);
-	DOREPLIFETIME(AItemBass, SectionIndex);
-	DOREPLIFETIME(AItemBass, PosX);
-	DOREPLIFETIME(AItemBass, PosY);
+	SetActorLocation(WorldLocation);
 }
--- Source/Necromancer/Item/ItemBass.h
+++ Source/Necromancer/Item/ItemBass.h
@@ -4,11 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Engine/DataTable.h"
+#include "GridInventory/ItemInstance/ItemInstanceComponent.h"
 #include "ItemBass.generated.h"
 
-class UItemInstance;
-struct FItemData;
+USTRUCT(BlueprintType)
+struct FItemInstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FName ItemID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 Count = 1;
+};
 
 UCLASS()
 class NECROMANCER_API AItemBass : public AActor
@@ -18,38 +27,23 @@ class NECROMANCER_API AItemBass : public AActor
 public:
 	AItemBass();
 
-	void CopyFromItemInstance(class UItemInstance* ItemInstance);
+	const FItemInstanceData& GetItemData() const { return ItemData; }
+
+	bool AddCount(int32 Amount);
+	bool RemoveCount(int32 Amount);
+	bool IsEmpty() const { return ItemData.Count <= 0; }
+
+	virtual void OnDropped(const FVector& WorldLocation);
+
+	UItemInstanceComponent* GetItemInstanceComponent() const
+	{
+		return ItemInstanceComponent;
+	}
 
 protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item|Data")
-	UDataTable* ItemDataTable;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	FItemInstanceData ItemData;
 
-	const FItemData* GetItemData() const;
-
-	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
-
-public:
-	UPROPERTY(Replicated, BlueprintReadOnly)
-	FGuid InstanceID;
-
-	UPROPERTY(Replicated, BlueprintReadOnly)
-	FName ItemID;
-
-	UPROPERTY(Replicated, BlueprintReadWrite)
-	float CurrentDurability;
-
-	UPROPERTY(Replicated, BlueprintReadWrite)
-	bool bRotated;
-
-	UPROPERTY(Replicated, BlueprintReadOnly)
-	FGuid OwnerItemGuid;
-
-	UPROPERTY(Replicated, BlueprintReadOnly)
-	int32 SectionIndex;
-
-	UPROPERTY(Replicated, BlueprintReadOnly)
-	int32 PosX;
-
-	UPROPERTY(Replicated, BlueprintReadOnly)
-	int32 PosY;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	UItemInstanceComponent* ItemInstanceComponent;
 };
