c8f1fe3ed228f6490cc2108655be74a00184acf7
ko gyeong tae <rudex1598@gmail.com>
2026-02-09 01:49:50 +0900

    [Add]장착 아이템 slot, equip, unequip 추가

--- Source/Necromancer/GridInventory/NecInventoryComponent.h
+++ Source/Necromancer/GridInventory/NecInventoryComponent.h
@@ -9,6 +9,16 @@
 /**
  * 
  */
+UENUM(BlueprintType)
+enum class EEquipmentSlot : uint8
+{
+	Head    UMETA(DisplayName = "Head"),
+	Body    UMETA(DisplayName = "Body"),
+	Legs    UMETA(DisplayName = "Legs"),
+	Bag     UMETA(DisplayName = "Bag"),
+	Weapon  UMETA(DisplayName = "Weapon")
+};
+
 class UItemInstance;
 
 UCLASS(ClassGroup = (Custom),meta = (BlueprintSpawnableComponent))
@@ -36,4 +46,60 @@ public:
 
 	UFUNCTION(BlueprintPure, Category = "Inventory")
 	UItemInstance* GetDefaultContainer() const;
+
+#pragma region Equipment
+private:
+	UPROPERTY(Replicated)
+	UItemInstance* HeadItem;
+
+	UPROPERTY(Replicated)
+	UItemInstance* BodyItem;
+
+	UPROPERTY(Replicated)
+	UItemInstance* LegsItem;
+
+	UPROPERTY(Replicated)
+	UItemInstance* BagItem;
+
+	UPROPERTY(Replicated)
+	UItemInstance* WeaponItem;
+
+	UPROPERTY(Replicated)
+	AActor* HeadActor;
+
+	UPROPERTY(Replicated)
+	AActor* BodyActor;
+
+	UPROPERTY(Replicated)
+	AActor* LegsActor;
+
+	UPROPERTY(Replicated)
+	AActor* BagActor;
+
+	UPROPERTY(Replicated)
+	AActor* WeaponActor;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Equipment")
+	UItemInstance* GetEquipmentItem(EEquipmentSlot Slot) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Equipment")
+	AActor* GetEquipmentActor(EEquipmentSlot Slot) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Equipment")
+	void EquipItem(UItemInstance* EquipItem);
+
+	UFUNCTION(BlueprintCallable, Category = "Equipment")
+	void UnequipItem(EEquipmentSlot Slot);
+
+	void SetEquipmentItem(EEquipmentSlot Slot, UItemInstance* NewItem);
+	void SetEquipmentActor(EEquipmentSlot Slot, AActor* NewActor);
+protected:
+	UFUNCTION(Server, Reliable)
+	void Server_EquipItem(UItemInstance* EquipItem);
+
+	void EquipItem_Internal(UItemInstance* EquipItem);
+#pragma endregion
+
+
 };
