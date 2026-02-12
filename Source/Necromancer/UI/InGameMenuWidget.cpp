b8850637b8493a84503a316689887175294ae87a
mw08081 <president304@naver.com>
2026-02-12 17:17:10 +0900

    [Add] Invite Friend button on WBP_InGameMuenu

--- Source/Necromancer/UI/InGameMenuWidget.cpp
+++ Source/Necromancer/UI/InGameMenuWidget.cpp
@@ -3,6 +3,8 @@
 #include "Kismet/KismetSystemLibrary.h"
 #include "Kismet/GameplayStatics.h"
 
+#include "GameInstance/NecAFGameInstance.h"
+
 void UInGameMenuWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
@@ -12,9 +14,24 @@ void UInGameMenuWidget::NativeConstruct()
 		ExitButton->OnClicked.RemoveDynamic(this, &UInGameMenuWidget::OnExitButtonClicked);
 		ExitButton->OnClicked.AddDynamic(this, &UInGameMenuWidget::OnExitButtonClicked);
 	}
+
+	if (InviteFriendButton)
+	{
+		InviteFriendButton->OnClicked.RemoveDynamic(this, &UInGameMenuWidget::OnInviteFriendButtonClicked);
+		InviteFriendButton->OnClicked.AddDynamic(this, &UInGameMenuWidget::OnInviteFriendButtonClicked);
+	}
 }
 
 void UInGameMenuWidget::OnExitButtonClicked()
 {
 	UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, true);
+}
+
+void UInGameMenuWidget::OnInviteFriendButtonClicked()
+{
+	UNecAFGameInstance* NecAFGameInstance = Cast<UNecAFGameInstance>(GetGameInstance());
+	if (NecAFGameInstance)
+	{
+		NecAFGameInstance->InviteFriend();
+	}
 }
