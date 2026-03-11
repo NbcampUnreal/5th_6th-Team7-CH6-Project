d87e0170835090da27e28e3b14816c22d90559e0
mw08081 <president304@naver.com>
2026-03-11 16:13:47 +0900

    [Add] 새로운 관전자 캠 이동 로직적용(클라 사망시에도 잘 적용)

--- Source/Necromancer/Character/NecPlayerCharacter.cpp
+++ Source/Necromancer/Character/NecPlayerCharacter.cpp
@@ -21,7 +21,7 @@
 #include "Engine/StaticMeshActor.h"
 #include "Item/Weapon_Item_Base.h"
 #include "Components/CapsuleComponent.h"
-
+#include "Net/UnrealNetwork.h"
 
 #include "WorldActor/Interactable.h"
 #include "WorldActor/InteractableActor.h"
@@ -66,12 +66,28 @@ ANecPlayerCharacter::ANecPlayerCharacter()
 	GetMesh()->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::AlwaysTickPoseAndRefreshBones;
 }
 
+
+void ANecPlayerCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, RemoteViewRot);
+}
+
 void ANecPlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	InventoryComponent->LoadEquipment();
 }
 
+void ANecPlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//DrawDebugString(GetWorld(), FVector(0, 0, 100), GetEnumText(GetLocalRole()), this, FColor::White, DeltaTime);
+
+	ReplicateRemoteViewRot();
+}
 
 FString ANecPlayerCharacter::GetEnumText(ENetRole _Role)
 {
@@ -90,15 +106,13 @@ FString ANecPlayerCharacter::GetEnumText(ENetRole _Role)
 	}
 }
 
-void ANecPlayerCharacter::Tick(float DeltaTime)
+void ANecPlayerCharacter::ReplicateRemoteViewRot()
 {
-	Super::Tick(DeltaTime);
-
-
-	DrawDebugString(GetWorld(), FVector(0, 0, 100), GetEnumText(GetLocalRole()), this, FColor::White, DeltaTime);
+	if (HasAuthority() && IsLocallyControlled())
+	{
+		RemoteViewRot = GetControlRotation();
+	}
 }
-
-
 
 void ANecPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
