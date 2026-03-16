802e7f14527a4c60f38f2400fd950e00a396df25
mw08081 <president304@naver.com>
2026-03-16 22:33:19 +0900

    [Add] IA_SpectatingTargetUp/Down

--- Source/Necromancer/Controller/NecPlayerController.h
+++ Source/Necromancer/Controller/NecPlayerController.h
@@ -26,6 +26,7 @@ protected:
 	
 
 	virtual void SetupInputComponent() override;
+
 	virtual void OnPossess(APawn* InPawn) override;
 	virtual void OnRep_PlayerState() override;
 	virtual void OnRep_Pawn() override;
@@ -61,6 +62,18 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
 	TObjectPtr<UInputAction> WheelAction;
 
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> SpectatingTargetUpAction;
+
+	UFUNCTION()
+	void SpectatingTargetUp();
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> SpectatingTargetDownAction;
+
+	UFUNCTION()
+	void SpectatingTargetDown();
+
 protected:
 	UPROPERTY(EditDefaultsOnly, Category = "UI")
 	TSubclassOf<UInGameHUDWidget> InGameHUDWidgetClass;
@@ -83,6 +96,10 @@ protected:
 public:
 	void OnStartGame();
 
+
+protected:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+public:
 	/// <summary>
 	/// 빙의 해제 및 카메라 이동 시퀀스 수행
 	/// </summary>
@@ -98,7 +115,7 @@ public:
 	void Client_HandleDeath(AActor* TargetToSpectate);
 
 	UFUNCTION(Server, Reliable)
-	void Server_RequestSpectatingTarget();
+	void Server_RequestSpectatingTarget(AActor* InSpectatingTarget, bool bIsUp);
 
 	/// <summary>
 	/// Client_HandleDeath() || UpdateSpectateRotation() 에서 호출
@@ -109,6 +126,7 @@ public:
 	void UpdateSpectateRotation();
 
 protected:
+	UPROPERTY(Replicated)
 	bool bIsSpectating = false;
 
 	FTimerHandle SpectateRotationTimerHandle;
